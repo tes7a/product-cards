3aa3be78551cccfdfcae8a93fe79b81e09f8dd52
Konstantine <ttes7a@gmail.com>
2025-02-27 13:35:08 +0200

    fixed the api request

--- src/api/cardsAPI.ts
+++ src/api/cardsAPI.ts
@@ -2,18 +2,21 @@ import axios from "axios";
 
 //dal
 const instance = axios.create({
-    baseURL: "https://run.mocky.io/v3/",
-})
+	baseURL: "https://products-cards-api.vercel.app",
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
 
 export const cardsAPI = {
-    getAllCards ()  {
-        return instance.get<GetCardType[]>("b7d36eea-0b3f-414a-ba44-711b5f5e528e")
-    }
-}
+	getAllCards() {
+		return instance.get<GetCardType[]>("");
+	},
+};
 
 //types
 export type GetCardType = {
-    name: string,
-    category: string,
-    price: number,
-}
+	name: string;
+	category: string;
+	price: number;
+};
